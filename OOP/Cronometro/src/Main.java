5121ad992c69d1a716fee297433ce536a454859f
FMarck07 <francesco.marchetto@iisviolamarchesini.edu.it>
2024-12-02 23:39:15 +0100

    Aggiunti esercizi

--- OOP/Cronometro/src/Main.java
+++ OOP/Cronometro/src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Cronometro cronometro = new Cronometro();
+        try { // Per far andare lo sleep
+            cronometro.avvia();
+            Thread.sleep(2500); // Attende 2,5 secondi
+            System.out.println("Durata corrente: " + cronometro.getDurataFormattata());
+            cronometro.ferma();
+            System.out.println("Durata finale: " + cronometro.getDurataFormattata());
+            cronometro.reset();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
