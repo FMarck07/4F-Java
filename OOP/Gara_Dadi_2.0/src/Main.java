18ecb87234a87669acb2f4d00a295b27295cd294
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-02-17 09:04:32 +0200

    Update Main.java

--- OOP/Gara_Dadi_2.0/src/Main.java
+++ OOP/Gara_Dadi_2.0/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Giocatore g1 = new Giocatore("Cicciogaer89");
-        Giocatore g2 = new Giocatore("nEGRO");
+        Giocatore g2 = new Giocatore("Polino");
         Partita p = new Partita(g1, g2);
 
         p.giocaPartita();
