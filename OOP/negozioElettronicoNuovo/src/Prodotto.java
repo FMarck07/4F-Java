e03b738d3622273b143a7dfd71abe3a2d05b282d
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-04-06 22:53:03 +0200

    Delete OOP/negozioElettronicoNuovo directory

--- OOP/negozioElettronicoNuovo/src/Prodotto.java
+++
@@ -1,9 +0,0 @@
-public interface Prodotto extends Cloneable {
-    int getCodice();
-    String getNome();
-    double getPrezzo();
-    int getQuantita();
-    void setPrezzo(double prezzo);
-    void setQuantita(int quantita);
-    Prodotto clone() throws CloneNotSupportedException;
-}
