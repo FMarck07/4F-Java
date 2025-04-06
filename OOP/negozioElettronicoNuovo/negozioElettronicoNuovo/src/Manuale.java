28319c3ce31f734c578176b80f70cb41515f385e
FMarck07 <150336290+FMarck07@users.noreply.github.com>
2025-04-06 22:52:17 +0200

    Add files via upload

---
+++ OOP/negozioElettronicoNuovo/negozioElettronicoNuovo/src/Manuale.java
@@ -0,0 +1,18 @@
+public class Manuale extends Libro {
+    private String argomento;
+
+    public Manuale(int codice, String nome, String descrizione, String autore,
+                   double prezzo, int quantita, String argomento) {
+        super(codice, nome, descrizione, autore, prezzo, quantita);
+        this.argomento = argomento;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Argomento : %s ", argomento) + super.toString();
+    }
+    @Override
+    public Manuale clone() throws CloneNotSupportedException {
+        return (Manuale) super.clone();
+    }
+}
